367b3a7fdaf187e975143e7b46f5f83b5eab86a3
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-17 17:45:53 +0100

    refactor: Refactor add to use rest parameter and reduce

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,9 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(...nums) {
   // Add a loop here
-  return a + b;
+  return nums.reduce((sum, num) => sum + num);
 }
 
 add(1, 2, 3, 4, 5);
